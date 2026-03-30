c6b5df9203054cc0c9cbfcd72cef65591032147a
jjj <kkk@ll.pl>
2026-03-30 19:48:52 +0200

    lab 7 complete

---
+++ lab7/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+mongoose
+	.connect('mongodb://127.0.0.1:27017/StudentDB')
+	.then(result => {
+		console.log('Połączono z bazą')
+	})
+	.catch(err => {
+		console.log('Nie można połączyć się z MongoDB. Błąd: ' + err)
+	})
+
+module.exports = mongoose
---
+++ lab7/server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const path = require('path')
+const handleBars = require('handlebars')
+const exphbs = require('express-handlebars')
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access')
+
+const app = express()
+const db = require('./db')
+
+app.use(
+	express.urlencoded({
+		extended: true,
+	}),
+)
+
+app.set('views', path.join(__dirname, './views/'))
+
+app.engine(
+	'hbs',
+	exphbs.engine({
+		handlebars: allowInsecurePrototypeAccess(handleBars),
+		extname: 'hbs',
+		defaultLayout: 'layout',
+		layoutsDir: __dirname + '/views',
+	}),
+)
+app.set('view engine', 'hbs')
+
+app.use('/', require('./controllers/StudentController'))
+
+app.listen(3000, () => {
+	console.log('Serwer nasłuchuje na porcie 3000')
+})
